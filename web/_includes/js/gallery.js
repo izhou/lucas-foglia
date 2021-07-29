695892331a6e64ca2ff45dd26f5abe35d0aeb5fa
Irene <i@ire.ne>
2021-07-29 01:16:58 -0700

    css and style refactor

--- web/_includes/js/gallery.js
+++ web/_includes/js/gallery.js
@@ -1,48 +1,48 @@
 var _gallery_index = 0;
-var _gallery_image_container;
-var _gallery_image_info;
-var _gallery_length = document.getElementsByClassName('project-gallery-image-container').length;
+var _gallery_photo_container;
+var _gallery_photo_info;
+var _gallery_length = document.getElementsByClassName('project-gallery-photo-container').length;
 
 function showContainer(container) {
   const links = document.getElementsByClassName(`project-info--link`);
-  const containers = document.getElementsByClassName('l-grid-content-container');
-  const project_info = document.getElementsByClassName('project-info--photo');
+  const containers = document.getElementsByClassName('grid-right-content');
+  const photo_info = document.getElementsByClassName('project-photo-info');
 
   Array.from(containers).forEach((elem) => { elem.classList.add('is-hidden'); });
   Array.from(links).forEach((elem) => { elem.classList.remove('container-active'); });
-  Array.from(project_info).forEach((elem) => { elem.classList.toggle('is-hidden', container != 'project-gallery'); });
+  Array.from(photo_info).forEach((elem) => { elem.classList.toggle('is-hidden', container != 'project-gallery'); });
 
   document.getElementById(container).classList.remove('is-hidden');
   document.querySelector(`[data-container="${container}"]`).classList.add('container-active');
 };
 
 function onIndexClick(index) {
-  setGalleryImage(index);
+  setGalleryPhoto(index);
   showContainer('project-gallery');
 }
 
-function setGalleryImage(index) {
-  if (_gallery_image_container) _gallery_image_container.classList.add('is-hidden');
-  if (_gallery_image_info) Array.from(_gallery_image_info).forEach((elem) => { elem.classList.add('is-hidden') });
+function setGalleryPhoto(index) {
+  if (_gallery_photo_container) _gallery_photo_container.classList.add('is-hidden');
+  if (_gallery_photo_info) Array.from(_gallery_photo_info).forEach((elem) => { elem.classList.add('is-hidden') });
 
 
-  _gallery_image_container = document.querySelector(`.project-gallery-image-container[data-gallery-index="${index}"]`);
-  _gallery_image_info = document.querySelectorAll(`.project-gallery-image-info[data-gallery-index='${index}']`);
+  _gallery_photo_container = document.querySelector(`.project-gallery-photo-container[data-gallery-index="${index}"]`);
+  _gallery_photo_info = document.querySelectorAll(`.project-photo-info-container[data-gallery-index='${index}']`);
 
-  _gallery_image_container.classList.remove('is-hidden');
-  Array.from(_gallery_image_info).forEach((elem) => { elem.classList.remove('is-hidden')});
+  _gallery_photo_container.classList.remove('is-hidden');
+  Array.from(_gallery_photo_info).forEach((elem) => { elem.classList.remove('is-hidden')});
 
   _gallery_index = index;
 }
 
 function goLeft() {
   let index = _gallery_index == 0 ? _gallery_length - 1 : _gallery_index - 1;
-  return setGalleryImage(index);
+  return setGalleryPhoto(index);
 }
 
 function goRight() {
   let index = _gallery_index == _gallery_length - 1 ? 0 : _gallery_index + 1;
-  return setGalleryImage(index);
+  return setGalleryPhoto(index);
 }
 
 document.onkeyup = function (e) {
@@ -56,4 +56,4 @@ document.onkeyup = function (e) {
   }
 };
 
-setGalleryImage(_gallery_index);
+setGalleryPhoto(_gallery_index);
