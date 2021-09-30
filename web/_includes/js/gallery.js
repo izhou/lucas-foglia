3fc0231fe980d13af80bb150fd9b73849887c197
Irene <i@ire.ne>
2021-09-30 14:04:52 -0400

    slideshow transitions

--- web/_includes/js/gallery.js
+++ web/_includes/js/gallery.js
@@ -13,90 +13,83 @@ class Gallery {
       this.caption_el = document.getElementById(caption_attr);
     }
 
-    this.setIndex(0);
+    this.setActivePhotoByIndex(0);
   }
 
-  setIndex(index) {
-    if (index == this.active_project_index) return;
-    let new_container = this.el.querySelector(`.gallery-photo-container[data-gallery-index="${index}"]`);
-    let old_container = this.active_photo_container;
+  setActivePhoto(container, index) {
+    if (this.active_photo_container) {
+      this.active_photo_container.classList.add('is-hidden');
+    }
 
-    if (old_container) old_container.classList.add('is-hidden');
-    new_container.classList.remove('is-hidden');
-    this.active_photo_container = new_container;
+    container.classList.remove('is-hidden');
+    this.active_photo_container = container;
+    this.active_photo_index = index;
 
-    // Set new active info 
+    // Set new active info
     if (this.caption_el) {
       if (this.active_photo_info) this.active_photo_info.classList.add('is-hidden');
       this.active_photo_info = this.caption_el.querySelector(`.gallery-photo-caption-container[data-gallery-index='${index}']`);
       this.active_photo_info.classList.remove('is-hidden');
     }
+  }
 
-    this.active_photo_index = parseInt(index);
+  setActivePhotoByIndex(index) {
+    if (this.active_photo_index == index) return;
+    let container = this.el.querySelector(`.gallery-photo-container[data-gallery-index="${index}"]`);
+
+    return this.setActivePhoto(container, index);
+  }
+
+  setActivePhotoByContainer(container) {
+    if (this.active_photo_container == container) return;
+    let index = parseInt(container.getAttribute('data-gallery-index'));
+
+    return this.setActivePhoto(container, index);
   }
 
   setSlideshow() {
-    // console.log('slideshow!' + this.active_photo_index);
-    let next_photo = this.getRightPhoto(this.active_photo_index);
-    let next_photo_image = next_photo.querySelector('.photo');
-    let next_index = next_photo.getAttribute('data-gallery-index');
-    lazySizes.loader.unveil(next_photo_image);
+    let next_index = this.getRightIndex(this.active_photo_index);
+    let next_photo = this.getPhotoFromIndex(next_index);
+    lazySizes.loader.unveil(next_photo);
     let gallery = this;
 
     this.slideshow =  setTimeout((e) => {
-      if (next_photo_image.classList.contains('lazyloaded')) {
-        this.setIndex(next_index);
-        this.setSlideshow();
+      if (next_photo.classList.contains('lazyloaded')) {
+        gallery.setActivePhotoByIndex(next_index);
+        gallery.setSlideshow();
       } else {
-        next_photo_image.addEventListener('lazyloaded', function (e) {
-          gallery.setIndex(next_index);
+        next_photo.addEventListener('lazyloaded', function (e) {
+          gallery.setActivePhotoByIndex(next_index);
           gallery.setSlideshow();
         }, { once: true })
       }
     }, 3000);
   }
 
-  transitionSlideshow(old_photo, new_photo) {
-
-  }
-
-  getLeftPhoto(index) {
-    let left_index = index == 0 ? this.length - 1 : index - 1;
-    return this.getPhotoFromIndex(left_index);
-  }
-
-  getRightPhoto(index) {
-    let right_index = index == this.length - 1 ?  0 : index + 1;
-    return this.getPhotoFromIndex(right_index);
-  }
-
-  setActivePhoto(index) {
-    if (index = this.active_index) return;
-
-    let new_photo = this.getPhotoFromIndex(index);
-    let old_photo = this.active_photo_container;
-
-    if (!new_photo) return;
-
-    if (old_container) old_container.classList.add('is-hidden');
-  }
-
   goLeft() {
-    let index = this.active_photo_index == 0 ? this.length - 1 : this.active_photo_index - 1;
-    return this.setIndex(index);
+    let index = this.getLeftIndex();
+    return this.setActivePhotoByIndex(index);
   }
 
   goRight() {
-    let index = this.active_photo_index == this.length - 1 ? 0 : this.active_photo_index + 1;
-    return this.setIndex(index);
+    let index = this.getRightIndex();
+    return this.setActivePhotoByIndex(index);
+  }
+
+  getPhotoFromIndex(index) {
+    return this.el.querySelector(`.gallery-photo-container[data-gallery-index="${index}"] > .photo`);
+  }
+
+  getLeftIndex() {
+    return this.active_photo_index == 0 ? this.length - 1 : this.active_photo_index - 1;
+  }
+
+  getRightIndex() {
+    return this.active_photo_index == this.length - 1 ? 0 : this.active_photo_index + 1;
   }
 
   getIndex() {
     return this.active_photo_index;
-  }
-
-  getPhotoFromIndex(index) {
-    return this.el.querySelector(`.gallery-photo-container[data-gallery-index="${index}"]`);
   }
 
   getLength() {
--- web/_includes/js/project.js
+++ web/_includes/js/project.js
@@ -25,7 +25,7 @@ let opening_hash = window.location.hash.slice(1);
 if (opening_hash == 'statement') {
   showContainer('project-statement');
 } else if (/^\d+$/.test(opening_hash) && opening_hash < gallery.getLength()) {
-  gallery.setIndex(opening_hash);
+  gallery.setActiveIndex(opening_hash);
   showContainer('project-gallery');
 } else if (/^i[\d]+/.test(opening_hash)) {
   let photo_index = opening_hash.slice(1);
@@ -37,7 +37,7 @@ if (opening_hash == 'statement') {
 }
 
 function onIndexClick(index) {
-  gallery.setIndex(index);
+  gallery.setActiveIndex(index);
   showContainer('project-gallery');
 }
 
