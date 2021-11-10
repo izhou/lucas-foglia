db5f68b7dd33db609e400aa558b3f7aa33e54de8
Irene <i@ire.ne>
2021-11-10 12:19:26 -0800

    some cleanup

--- web/_includes/js/base.js
+++ web/_includes/js/base.js
@@ -1,15 +1,8 @@
-document.getElementById('dark-mode').checked = document.documentElement.getAttribute('data-mode') == "dark";
-
-document.getElementById('dark-mode').addEventListener('input', (e) => {
-  let mode = e.target.checked ? "dark" : "light";
-  localStorage.setItem('mode', mode);
-  document.documentElement.setAttribute('data-mode', mode);
-});
-
+let buttons = document.querySelector('.buttons');
 let _hideButtons;
 let _scheduled;
-let buttons = document.querySelector('.buttons');
 
+// Hide buttons unless mousemove
 document.addEventListener('mousemove', (e) => setButtonTimeout());
 
 function setButtonTimeout(timeout = 2000) {
@@ -26,3 +19,17 @@ function setButtonTimeout(timeout = 2000) {
 }
 
 setButtonTimeout(5000);
+
+// Set dark mode button
+let dark_mode_btn = document.getElementById('dark-mode');
+
+if (dark_mode_btn) {
+  dark_mode_btn.checked = document.documentElement.getAttribute('data-mode') == "dark";
+
+  dark_mode_btn.addEventListener('input', (e) => {
+    let mode = e.target.checked ? "dark" : "light";
+    localStorage.setItem('mode', mode);
+    document.documentElement.setAttribute('data-mode', mode);
+  });
+}
+
